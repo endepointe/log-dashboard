783ce24bc08dd7d95f349215db656e73719db388
endepointe <endepointe@gmail.com>
2024-08-11 18:33:23 -0700

    log-analysis lib working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+use log_analysis::{
+    zeek::zeek_search_params::ZeekSearchParamsBuilder,     
+    zeek::zeek_log::ZeekLog,
+};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let params = ZeekSearchParamsBuilder::default()
+        .path_prefix("~/dev/log-analysis/zeek-test-logs")
+        .start_date("2024-07-02")
+        .src_ip("43.134.231.178")
+        .build()
+        .unwrap();
+    dbg!(&params);
+    let mut log = ZeekLog::new();
+    let res = log.search(&params);
+    let res = res.unwrap();
+    format!("Hello, {}! You've been greeted from Rust! {:?}", name, res)
 }
 
 fn main() {
